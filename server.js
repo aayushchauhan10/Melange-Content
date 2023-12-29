2343f683fb94042c57246bdf293e840016bdb174
aayushchauhan10 <aayushchauhanrajput47@gmail.com>
2023-12-29 13:21:53 +0530

    debugged

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-import bodyParser from 'body-parser';
+import express from "express";
+import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,56 +9,55 @@ app.use(bodyParser.json());
 
 // CORS middleware to allow requests from your React app (update with your frontend URL)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://melange-content-page.netlify.app');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://melange-content-page.netlify.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.post('/send-email', async (req, res) => {
-    const {
-        name,
-        email,
-        phone,
-        need,
-        budget,
-        message
-    } = req.body;
+app.post("/send-email", async (req, res) => {
+  const { name, email, phone, need, budget, message } = req.body;
 
-    // Create a nodemailer transporter
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail', // Update with your email service (e.g., 'gmail')
-        auth: {
-            user: 'ai.ronycode@gmail.com', // Update with your email address
-            pass: 'msgx chrq cokf qlhj' // Update with your email password
-        }
-    });
+  // Create a nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: "Gmail", // Update with your email service (e.g., 'gmail')
+    auth: {
+      user: "ai.ronycode@gmail.com", // Update with your email address
+      pass: "msgx chrq cokf qlhj", // Update with your email password
+    },
+  });
 
-    // Define email options
-    const mailOptions = {
-        from: `${email}`,
-        to: 'ai.ronycode@gmail.com', // Update with your recipient email address
-        subject: 'New Form Submission',
-        text: `
+  // Define email options
+  const mailOptions = {
+    from: `${name}`,
+    to: "ai.ronycode@gmail.com", // Update with your recipient email address
+    subject: "New Form Submission",
+    text: `
       Name: ${name}
       Email: ${email}
       Phone: ${phone}
       Need: ${need}
       Budget: ${budget}
       Message: ${message}
-    `
-    };
+    `,
+  };
 
-    try {
-        // Send email
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent:', info.messageId);
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Error sending email:', error);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
+  try {
+    // Send email
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.messageId);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+  console.log($`{PORT}`);
 });
